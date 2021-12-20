3b67e059e4801683a48e6e25daa16dc9c0decaae
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2021-12-20 19:28:46 +0000

    rebase part 1

--- src/main/java/gregtech/integration/jei/recipe/GTRecipeWrapper.java
+++ src/main/java/gregtech/integration/jei/recipe/GTRecipeWrapper.java
@@ -124,6 +124,8 @@ public class GTRecipeWrapper implements IRecipeWrapper {
             tooltip.add(I18n.format("gregtech.recipe.chance", chance, boost));
         } else if (notConsumed) {
             tooltip.add(I18n.format("gregtech.recipe.not_consumed"));
+        } else if (recipe.getOutputs().get(0).hasTagCompound() && ((ItemStack) ingredient).isItemEqual(new ItemStack(recipe.getOutputs().get(0).getSubCompound("asslineOutput")))) {
+            tooltip.add(I18n.format("gregtech.recipe.assline_scanner"));
         }
     }
 
