c827455cf5ba3915939a2dffb89c33d97f016745
tech22 <37029404+TechLord22@users.noreply.github.com>
2022-01-27 22:56:34 -0500

    set HT false

--- src/main/java/gregtech/api/GTValues.java
+++ src/main/java/gregtech/api/GTValues.java
@@ -159,7 +159,7 @@ public class GTValues {
     /**
      * Used to tell if any high-tier machine (UHV+) was registered.
      */
-    public static boolean HT = true;
+    public static boolean HT = false;
 
     public static Supplier<Boolean> FOOLS = () -> {
         String[] yearMonthDay = LocalDate.now().toString().split("-");
